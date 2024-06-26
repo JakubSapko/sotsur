523f1f56be46fa70ad8dd59849b68850a47c0e66
JakubSapko <jakub.sapko1@gmail.com>
2024-06-26 13:10:33 +0200

    hello world

--- src/main.rs
+++ src/main.rs
@@ -9,8 +9,19 @@ fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
+static HELLO_WORLD: &[u8] = b"HELLO WORLD!";
+
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     // default entry point, linker looks for a _start function by default
+
+    let vga_buff = 0xb8000 as *mut u8;
+
+    for (i, &byte) in HELLO_WORLD.iter().enumerate() {
+        unsafe {
+            *vga_buff.offset(i as isize * 2) = byte;
+            *vga_buff.offset(i as isize * 2 + 1) = 0xb; // cyan
+        }
+    }
     loop {}
 }
