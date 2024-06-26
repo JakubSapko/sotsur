62f5ca51323ebe6f0c6dbf9db2f502cb481128a9
JakubSapko <jakub.sapko1@gmail.com>
2024-06-26 10:40:47 +0200

    crates configuration

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,7 @@
 
 use core::panic::PanicInfo;
 
+// in default rust panic handler is a part of standard library which we can't use
 #[panic_handler]
 fn panic(_info: &PanicInfo) -> ! {
     loop {}
@@ -10,5 +11,6 @@ fn panic(_info: &PanicInfo) -> ! {
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    // default entry point, linker looks for a _start function by default
     loop {}
 }
